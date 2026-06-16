03641b0c829ba10809d8318c0caacba94fc8c08c
Cooldude1259 <lucas.chrisopoulos@gmail.com>
2026-06-16 17:45:51 +0930

    Add Google and Github Signins, even though no use

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -3,13 +3,55 @@ import SwiftUI
 struct ContentView: View {
     @State private var count = 0
 
+    func signInWithGitHub() async {
+        do {
+            try await supabase.auth.signInWithOAuth(provider: .github)
+        } catch {
+            print("Sign-in failed: /(error)")
+        }
+    }
+
+    func signInWithGoogle() async {
+        do {
+            try await supabase.auth.signInWithOAuth(provider: .google)
+        } catch {
+            print("Sign-in failed: /(error)")
+        }
+    }
+
     var body: some View {
         VStack(spacing: 20) {
             
             
-            Text("Hello, SwiftUI!")
+            Text("Welcome to ConnectEd!")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+
+            Button {
+                Task { await signInWithGitHub() }
+
+            } label: {
+                Text("Sign in with GitHub")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button {
+                Task { await signInWithGoogle() }
+
+            } label: {
+                Text("Sign in with Google")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
             
             Text("Button pressed: \(count) times")
                 .font(.body)
